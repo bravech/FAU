4154696b1be7882cdf1f5efddf513f692eba9f0d
Charlie Phillips <cgp246@gmail.com>
2018-06-14 14:00:10 -0400

    Moved rewrite into original version

--- arduSerial.py
+++ arduSerial.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python
-#
-# Redirect data from a TCP/IP connection to a serial port and vice versa.
-#
-# (C) 2002-2016 Chris Liechti <cliechti@gmx.net>
-#
-# SPDX-License-Identifier:    BSD-3-Clause
-
 import sys
 import socket
 import serial
@@ -13,9 +5,10 @@ import serial.threaded
 import time
 
 
-class SerialToNet(serial.threaded.Protocol): #Used as a helper class for serial.threaded
+# Used as a helper class for serial.threaded.ReaderThread
+# Implements necessary classes for serial.threaded.Protocol
+class SerialToNet(serial.threaded.Protocol):
     """serial->socket"""
-
     def __init__(self, target):
         self.socket = None
         self.target = target
@@ -28,11 +21,11 @@ class SerialToNet(serial.threaded.Protocol): #Used as a helper class for serial.
             self.socket.sendto(data, self.target)
 
 
-if __name__ == '__main__':  # noqa
-    import argparse
+if __name__ == '__main__':
+    import argparse  # Useful module for argument parsing
 
     parser = argparse.ArgumentParser(
-        description='Simple Serial to UDP Sockets redirector.')
+            description='Serial to UDP Sockets redirector.')
 
     parser.add_argument(
         'SERIALPORT',
@@ -51,159 +44,82 @@ if __name__ == '__main__':  # noqa
         help='suppress non error messages',
         default=False)
 
-    parser.add_argument(
-        '--develop',
-        action='store_true',
-        help='Development mode, prints Python internals on errors',
-        default=False)
-
-    group = parser.add_argument_group('serial port')
-
-    group.add_argument(
-        "--parity",
-        choices=['N', 'E', 'O', 'S', 'M'],
-        type=lambda c: c.upper(),
-        help="set parity, one of {N E O S M}, default: N",
-        default='N')
-
-    group.add_argument(
-        '--rtscts',
-        action='store_true',
-        help='enable RTS/CTS flow control (default off)',
-        default=False)
-
-    group.add_argument(
-        '--xonxoff',
-        action='store_true',
-        help='enable software flow control (default off)',
-        default=False)
-
-    group.add_argument(
-        '--rts',
-        type=int,
-        help='set initial RTS line state (possible values: 0, 1)',
-        default=None)
-
-    group.add_argument(
-        '--dtr',
-        type=int,
-        help='set initial DTR line state (possible values: 0, 1)',
-        default=None)
-
     group = parser.add_argument_group('network settings')
 
-    exclusive_group = group.add_mutually_exclusive_group()
 
-    exclusive_group.add_argument(
+    group.add_argument(
         '-P', '--localIP',
         help='local UDP IP (default localhost)',
         default='')
-    
-    exclusive_group.add_argument(
+
+    group.add_argument(
         '-p', '--localport',
         type=int,
         help='local UDP port (default 7777)',
         default=7777)
-    
-    exclusive_group.add_argument(
+
+    group.add_argument(
             '-R', '--remoteIP',
             help='remote UDP IP (default localhost)',
             default=''
             )
-    
-    exclusive_group.add_argument(
+
+    group.add_argument(
             '-r', '--remoteport',
             type=int,
             help='remote UDP port (default 8888)',
             default=8888
             )
 
-
+    # Parses args and puts them in an accessable format
     args = parser.parse_args()
 
-    # connect to serial port
+    # Connect to serial port, but do not open
     ser = serial.serial_for_url(args.SERIALPORT, do_not_open=True)
     ser.baudrate = args.BAUDRATE
-    ser.parity = args.parity #default: N
-    ser.rtscts = args.rtscts #default: False
-    ser.xonxoff = args.xonxoff #default: False
 
-    if args.rts is not None:
-        ser.rts = args.rts
-
-    if args.dtr is not None:
-        ser.dtr = args.dtr
-
+    # Print info when starting
     if not args.quiet:
-        sys.stderr.write(
-            '--- UDP to Serial redirect on {p.name}  {p.baudrate},{p.bytesize},{p.parity},{p.stopbits} ---\n'
-            '--- type Ctrl-C / BREAK to quit\n'.format(p=ser))
+        sys.stderr.write('--- UDP to Serial redirect on '
+                         '{p.name} at {p.baudrate}, '
+                         'listening on {a.localIP}:{a.localport}, '
+                         'broadcasting to {a.remoteIP}:{a.remoteport} ---\n'.format(p=ser, a=args))
+        sys.stderr.write('--- type Ctrl-C / BREAK to quit ---\n')
 
+    # Open serial port
     try:
         ser.open()
     except serial.SerialException as e:
         sys.stderr.write('Could not open serial port {}: {}\n'.format(ser.name, e))
         sys.exit(1)
 
-    ser_to_net = SerialToNet(('127.0.0.1', 12345))
+    # Create helper class for serial.threaded, start threaded serial worker
+    ser_to_net = SerialToNet((args.remoteIP, args.remoteport))
     serial_worker = serial.threaded.ReaderThread(ser, ser_to_net)
     serial_worker.start()
 
-    if not args.client:
-        srv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        #srv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        srv.bind(('', args.localport))
-        #srv.listen(1)
-    try:
-        intentional_exit = False
-        while True:
-            if args.client:
-                host, port = args.client.split(':')
-                sys.stderr.write("Opening connection to {}:{}...\n".format(host, port))
-                client_socket = socket.socket()
-                try:
-                    client_socket.connect((host, int(port)))
-                except socket.error as msg:
-                    sys.stderr.write('WARNING: {}\n'.format(msg))
-                    time.sleep(5)  # intentional delay on reconnection as client
-                    continue
-                sys.stderr.write('Connected\n')
-                client_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-                #~ client_socket.settimeout(5)
-            else:
-                sys.stderr.write('Waiting for connection on {}...\n'.format(args.localport))
-            try:
-                #ser_to_net.socket = client_socket
-                # enter network <-> serial loop
-                ser_to_net.socket = srv
-                while True:
-                    try:
-                        #data = client_socket.recv(1024)
-                        data = srv.recv(1024)
-                        #if not data:
-                        #    break
-                        ser.write(data)                 # get a bunch of bytes and send them
-                    except socket.error as msg:
-                        if args.develop:
-                            raise
-                        sys.stderr.write('ERROR: {}\n'.format(msg))
-                        # probably got disconnected
-                        break
-            except KeyboardInterrupt:
-                intentional_exit = True
+    # Start configuring socket
+    soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    soc.bind((args.localIP, args.localport))
+    
+    # network <-> serial loop
+    intentional_exit = False
+    ser_to_net.socket = soc
+    while True:
+        try: 
+            data = soc.recv(1024)  # Recieve data from socket in chunks of 1024 bytes
+            ser.write(data)
+        except KeyboardInterrupt:
+            intentional_exit = True
+            if not args.quiet:
+                sys.stderr.write('\n--- exit ---\n')
+            serial_worker.stop()
+            break
+        
+        except socket.error as msg:
+            if args.develop:
                 raise
-            except socket.error as msg:
-                if args.develop:
-                    raise
-                sys.stderr.write('ERROR: {}\n'.format(msg))
-            finally:
-                ser_to_net.socket = None
-                sys.stderr.write('Disconnected\n')
-                client_socket.close()
-                if args.client and not intentional_exit:
-                    time.sleep(5)  # intentional delay on reconnection as client
-    except KeyboardInterrupt:
-        pass
-
-    sys.stderr.write('\n--- exit ---\n')
-    serial_worker.stop()
+            if not args.quiet:
+                sys.stderr.write('Error: {}\n'.format(msg))
+            # Disconnect if socket error occurs
+            break
