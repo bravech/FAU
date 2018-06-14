dab5900c5e3ed1976d67bb0be484f374215fe842
Charlie Phillips <cgp246@gmail.com>
2018-06-14 13:59:31 -0400

    Finished rewrite

--- rewrite.py
+++ rewrite.py
@@ -5,11 +5,10 @@ import serial.threaded
 import time
 
 
-#Used as a helper class for serial.threaded.ReaderThread
-#Implements necessary classes for serial.threaded.Protocol
-class SerialToNet(serial.threaded.Protocol): 
+# Used as a helper class for serial.threaded.ReaderThread
+# Implements necessary classes for serial.threaded.Protocol
+class SerialToNet(serial.threaded.Protocol):
     """serial->socket"""
-
     def __init__(self, target):
         self.socket = None
         self.target = target
@@ -23,9 +22,10 @@ class SerialToNet(serial.threaded.Protocol):
 
 
 if __name__ == '__main__':
-    import argparse #Useful module for argument parsing
+    import argparse  # Useful module for argument parsing
 
-    parser = argparse.ArgumentParser(description='Serial to UDP Sockets redirector.')
+    parser = argparse.ArgumentParser(
+            description='Serial to UDP Sockets redirector.')
 
     parser.add_argument(
         'SERIALPORT',
@@ -46,52 +46,80 @@ if __name__ == '__main__':
 
     group = parser.add_argument_group('network settings')
 
-    exclusive_group = group.add_mutually_exclusive_group()
 
-    exclusive_group.add_argument(
+    group.add_argument(
         '-P', '--localIP',
         help='local UDP IP (default localhost)',
         default='')
-    
-    exclusive_group.add_argument(
+
+    group.add_argument(
         '-p', '--localport',
         type=int,
         help='local UDP port (default 7777)',
         default=7777)
-    
-    exclusive_group.add_argument(
+
+    group.add_argument(
             '-R', '--remoteIP',
             help='remote UDP IP (default localhost)',
             default=''
             )
-    
-    exclusive_group.add_argument(
+
+    group.add_argument(
             '-r', '--remoteport',
             type=int,
             help='remote UDP port (default 8888)',
             default=8888
             )
 
+    # Parses args and puts them in an accessable format
+    args = parser.parse_args()
 
-    args = parser.parse_args() #Parses args and puts them in an accessable format
-
-
-    #Connect to serial port
+    # Connect to serial port, but do not open
     ser = serial.serial_for_url(args.SERIALPORT, do_not_open=True)
     ser.baudrate = args.BAUDRATE
 
+    # Print info when starting
     if not args.quiet:
-        sys.stderr.write('--- UDP to Serial redirect on {p.name} {p.baudrate}, listening on {a.localIP}:{a.localport}, broadcasting to {a.remoteIP}:{a.remoteport} ---\n'.format(p=ser, a=args))
+        sys.stderr.write('--- UDP to Serial redirect on '
+                         '{p.name} at {p.baudrate}, '
+                         'listening on {a.localIP}:{a.localport}, '
+                         'broadcasting to {a.remoteIP}:{a.remoteport} ---\n'.format(p=ser, a=args))
         sys.stderr.write('--- type Ctrl-C / BREAK to quit ---\n')
 
+    # Open serial port
     try:
         ser.open()
     except serial.SerialException as e:
         sys.stderr.write('Could not open serial port {}: {}\n'.format(ser.name, e))
         sys.exit(1)
 
+    # Create helper class for serial.threaded, start threaded serial worker
+    ser_to_net = SerialToNet((args.remoteIP, args.remoteport))
+    serial_worker = serial.threaded.ReaderThread(ser, ser_to_net)
+    serial_worker.start()
+
+    # Start configuring socket
+    soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    soc.bind((args.localIP, args.localport))
     
-
-
-
-    
+    # network <-> serial loop
+    intentional_exit = False
+    ser_to_net.socket = soc
+    while True:
+        try: 
+            data = soc.recv(1024)  # Recieve data from socket in chunks of 1024 bytes
+            ser.write(data)
+        except KeyboardInterrupt:
+            intentional_exit = True
+            if not args.quiet:
+                sys.stderr.write('\n--- exit ---\n')
+            serial_worker.stop()
+            break
+        
+        except socket.error as msg:
+            if args.develop:
+                raise
+            if not args.quiet:
+                sys.stderr.write('Error: {}\n'.format(msg))
+            # Disconnect if socket error occurs
+            break
